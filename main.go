efcb108fd67eb801944561839efff0080b7199d5
Smackem <sm4ckem@outlook.com>
2017-02-27 22:11:05 +0100

    started on http server

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
 import "fmt"
+import "net/http"
+
+const (
+	port = 9090
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
-	fmt.Printf("%d\n", 123)
-	fmt.Printf("hello, world\n")
+	fmt.Printf("Listening on port %d\n", port)
+
+	pattern := fmt.Sprintf(":%d", port)
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(pattern, nil)
 }
